fc6d3a5c03b7c6e78db6a907c11664b586791ff7
jl0844 <jl0844@unt.edu>
2020-10-10 17:59:00 -0500

    CR / CRLF error with prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,10 @@ module.exports = {
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
 };
