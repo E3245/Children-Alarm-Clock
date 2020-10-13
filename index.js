678523d6ab7df8eeaf36306f2f1dede92837412c
E3245 <gibbly@zoho.com>
2020-10-13 12:38:45 -0500

    Added 'react-native-gesture-handler' import statement [REQUIRED]

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Clock from './components/Clock'
 import {name as appName} from './app.json';
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);
