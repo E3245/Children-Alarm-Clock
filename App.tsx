b63c7449232a1d99d339d906ae1be6e32fbc3927
E3245 <gibbly@zoho.com>
2020-10-13 12:46:39 -0500

    Added basic tabs to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,24 +1,58 @@
   
 import React from "react";
-import { useColorScheme } from "react-native";
+import { useColorScheme, Text, View } from "react-native";
 import Clock from "./components/Clock";
 import styled, { ThemeProvider } from "styled-components/native";
 import { darkTheme, lightTheme } from "./themes";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Test</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function AppTabs()
+{
+  return (
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Settings" component={SettingsScreen} />
+  </Tab.Navigator>
+  );
+}
 
 const App = () => {
   const theme = useColorScheme() === "dark" ? darkTheme : lightTheme;
 
   return (
-    <ThemeProvider theme={theme}>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" />
       <SafeAreaView>
         <ScrollView
           centerContent={true}
           contentInsetAdjustmentBehavior="automatic">
           <Clock />
-        </ScrollView>
-      </SafeAreaView>
-    </ThemeProvider>
+       </ScrollView>
+     </SafeAreaView>
+     </ThemeProvider>   
+     <AppTabs />
+    </NavigationContainer>
   );
 };
 
