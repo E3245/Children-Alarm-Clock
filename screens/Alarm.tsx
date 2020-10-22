280803b8f282eebf6e5182acb5dd4d554032296a
jl0844 <5272660+TheJohnLong@users.noreply.github.com>
2020-10-22 00:06:32 -0500

    Added temp alarm page by copying timers

--- screens/Alarm.tsx
+++ screens/Alarm.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import {useColorScheme, Text, View} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import {darkTheme, lightTheme} from '../themes';
-import {styles} from '../components/stylesheet';
+import {ScrollView, styles} from '../components/stylesheet';
+import TimerComponentSimple from '../components/Timer/Timer';
 
 const AlarmScreen = () => {
   const theme = useColorScheme() === 'dark' ? darkTheme : lightTheme;
 
   return (
-    <View style={styles.centered}>
-      <ThemeProvider theme={theme}>
-        <Text>Alarm!</Text>
-      </ThemeProvider>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Timer Page</Text>
+      </View>
+      <ScrollView style={styles.TimerScroll}>
+        <View style={styles.centered}>
+          <TimerComponentSimple endTime={new Date(100000000)} />
+          <TimerComponentSimple endTime={new Date(200000000)} />
+        </View>
+      </ScrollView>
+    </ThemeProvider>
   );
 };
 
