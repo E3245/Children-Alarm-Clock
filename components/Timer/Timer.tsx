120b1569c2c708bb75d14912179cff7ea503f5b9
jl0844 <5272660+TheJohnLong@users.noreply.github.com>
2020-11-08 18:01:24 -0600

    migrated timer to a class component, and fixed many, many, many bugs

--- components/Timer/Timer.tsx
+++ components/Timer/Timer.tsx
@@ -1,17 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import {View, Button} from 'react-native';
 import {getTimeTo, isTimePast, formatTime} from '../../helpers/time';
 import {styles} from '../stylesheet';
 import Svg, {Text, Rect} from 'react-native-svg';
 
 export type TimerProps = {
-  // Length of the timer in seconds
+  // The amount of time the timer will run for when reset
   amountTime: number;
   name: string;
   color: string;
   uuid: string;
   handleChange: CallableFunction;
-  // State initial
+  // The time remaining on the timer in milliseconds (if it is currently stopped)
+  // The time the timer will end at in ms since epoch (if it is currently running)
+  // This var stores different values depending on the running boolean
   time: number;
   running: boolean;
 };
@@ -25,122 +27,170 @@ type OtherProps = {
 // How to use this component
 // <Timer endTime={new Date(1602612000)} />
 
-export const TimerComponentSimple = ({
-  // The amount of time the timer will run for when reset
-  amountTime = -1,
-  name,
-  color,
-  handleChange,
-  uuid,
-  // The time remaining on the timer in milliseconds (if it is currently stopped)
-  // The time the timer will end at in ms since epoch (if it is currently running)
-  // This var stores different values depending on the running boolean
-  time = -1,
-  // If the timer is currently counting down
-  running,
-}: TimerProps) => {
-  if (time === -1) {
-    throw new Error('time must be set');
+export class TimerComponentSimple extends React.Component<TimerProps> {
+  state = {
+    timeState: -1,
+    isRunning: false,
+    renderTime: -1,
+  };
+
+  // The id for the interval that ticks every second
+  private intervalID: any;
+
+  constructor(props: TimerProps) {
+    super(props);
+    this.state.timeState = props.time;
+    this.state.isRunning = props.running;
+
+    // Set the render time based on isRunning and if the time var is in the past
+    if (props.running && isTimePast(props.time)) {
+      this.state.renderTime = 0;
+    } else if (props.running) {
+      this.state.renderTime = getTimeTo(props.time);
+    } else {
+      // In this case the timer is stopped and time is storing the remaining time
+      this.state.renderTime = props.time;
+    }
   }
 
-  // Set the end time based on either the remaining time
-  let [timeState, setTime] = useState(time);
-  let [isRunning, setIsRunning] = useState(running);
-  let [renderTime, setRenderTime] = useState(running ? getTimeTo(time) : time);
-
-  useEffect(() => {
-    let interval: any = null;
-    if (isRunning && isTimePast(timeState)) {
-      console.log('Timer Ended!');
-      setRenderTime(0);
-      setIsRunning(false);
-      clearInterval(interval);
-    } else if (isRunning) {
-      interval = setInterval(() => {
-        setRenderTime(getTimeTo(timeState));
-      }, 100);
+  handleToggle = () => {
+    // Check if we should reset instead of toggle
+    if (this.state.timeState === 0) {
+      this.reset();
+      return;
     }
 
-    return () => clearInterval(interval);
-  }, [isRunning, timeState, renderTime]);
-
-  const toggleTimer = () => {
-    // Check if needs to reset ()
-    if (!isRunning && renderTime === 0) {
-      setTime(amountTime);
-      setRenderTime(amountTime);
-    } else if (isRunning) {
-      // Set the time remaining based on how far the timer has progressed
-      setTime(getTimeTo(timeState));
-      // Stop timer
-      setIsRunning(false);
+    if (this.state.isRunning) {
+      this.stop();
     } else {
-      // Start timer
-      // Set the end time based on the time remaining
-      setTime(Date.now() + timeState);
-      setIsRunning(true);
+      this.start();
     }
   };
 
+  componentDidUpdate(prevProps: any, prevState: any) {
+    if (this.state.isRunning !== prevState.isRunning) {
+      this.save();
+    } else if (this.state.timeState !== prevState.timeState) {
+      this.save();
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.isRunning) {
+      this.intervalID = setInterval(() => this.tick(), 1000);
+    }
+  }
+
+  tick() {
+    if (!this.state.isRunning) {
+      console.log('Timer tried to tick while stopped');
+      return;
+    }
+
+    // Check if the clock should stop
+    if (isTimePast(this.state.timeState)) {
+      console.log('Timer Ended!');
+      // Force the render time to read 0
+      this.setState({renderTime: 0, isRunning: false, timeState: 0});
+      // Stop the ticking
+      clearInterval(this.intervalID);
+    } else {
+      this.setState({renderTime: getTimeTo(this.state.timeState)});
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  start() {
+    console.log('Start');
+    // Save the time that the timer will end at
+    this.setState({
+      isRunning: true,
+      timeState: Date.now() + this.state.timeState,
+    });
+
+    this.intervalID = setInterval(() => {
+      this.tick();
+    }, 100);
+  }
+
+  stop() {
+    console.log('Stop');
+    // Save the time remaining on the timer
+    this.setState({
+      isRunning: false,
+      timeState: getTimeTo(this.state.timeState),
+    });
+
+    clearInterval(this.intervalID);
+  }
+
+  reset() {
+    console.log('Reset');
+    this.setState({
+      isRunning: false,
+      renderTime: this.props.amountTime,
+      timeState: this.props.amountTime,
+    });
+  }
+
   // Call the parent to save the data
-  const save = () => {
+  save = () => {
+    console.log('SAVE');
     // Values that may have changed
     // running
     // remainingtime
-    let timer: TimerProps = {
-      amountTime: amountTime,
-      name: name,
-      color: color,
-      handleChange: handleChange,
-      uuid: uuid,
-      // State changes
-      time: timeState,
-      running: isRunning,
-    };
+    let timer = {...this.props};
+    timer.running = this.state.isRunning;
+    timer.time = this.state.timeState;
 
     console.log(timer.running);
 
-    handleChange(timer);
+    this.props.handleChange(timer);
   };
 
-  return (
-    <View style={styles.TimerContainer}>
-      <Svg width="90%" height="70">
-        <Rect
-          x="0"
-          y="0"
-          rx="10"
-          ry="10"
-          width="100%"
-          height="100%"
-          stroke="black"
-          fill={color}
-          transform="translate(0,0)"
+  render = () => {
+    return (
+      <View style={styles.TimerContainer}>
+        <Svg width="90%" height="70">
+          <Rect
+            x="0"
+            y="0"
+            rx="10"
+            ry="10"
+            width="100%"
+            height="100%"
+            stroke="black"
+            fill={this.props.color}
+            transform="translate(0,0)"
+          />
+          <Text
+            fill="black"
+            stroke="black"
+            fontSize="300%"
+            fontWeight="bold"
+            x="50%"
+            y="50%"
+            textAnchor="middle">
+            {formatTime(new Date(this.state.renderTime))}
+          </Text>
+          <Text
+            fill="black"
+            stroke="black"
+            fontSize="200%"
+            x="50%"
+            y="90%"
+            textAnchor="middle">
+            {this.props.name}
+          </Text>
+        </Svg>
+        <Button
+          title={this.state.renderTime === 0 ? 'Reset' : 'Toggle'}
+          onPress={this.handleToggle}
         />
-        <Text
-          fill="black"
-          stroke="black"
-          fontSize="300%"
-          fontWeight="bold"
-          x="50%"
-          y="50%"
-          textAnchor="middle">
-          {formatTime(new Date(renderTime))}
-        </Text>
-        <Text
-          fill="black"
-          stroke="black"
-          fontSize="200%"
-          x="50%"
-          y="90%"
-          textAnchor="middle">
-          {name}
-        </Text>
-      </Svg>
-      <Button
-        title={renderTime === 0 ? 'Reset' : 'Toggle'}
-        onPress={() => toggleTimer()}
-      />
-    </View>
-  );
-};
+      </View>
+    );
+  };
+}
