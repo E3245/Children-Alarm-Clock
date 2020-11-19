aae14e5e2d36b9dcf5d513fe3c66579febf5815f
jl0844 <5272660+TheJohnLong@users.noreply.github.com>
2020-11-19 11:30:46 -0600

    remove timer button from screen

--- screens/Timer.tsx
+++ screens/Timer.tsx
@@ -19,28 +19,28 @@ const TimerScreen = () => {
         </View>
       </ScrollView>
       <View>
-        <View style={styles.AddButtonBottom}>
+        {/* <View style={styles.AddButtonBottom}>
           <AddTimerButton />
-        </View>
+        </View> */}
       </View>
     </ThemeProvider>
   );
 };
 
-// Button that allows us to add a timer
-class AddTimerButton extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!');
-  }
+// // Button that allows us to add a timer
+// class AddTimerButton extends Component {
+//   _onPressButton() {
+//     Alert.alert('You tapped the button!');
+//   }
 
-  render() {
-    return (
-      <View style={styles.buttonContainer}>
-        <Button onPress={this._onPressButton} title="Add Timer" />
-      </View>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <View style={styles.buttonContainer}>
+//         <Button onPress={this._onPressButton} title="Add Timer" />
+//       </View>
+//     );
+//   }
+// }
 
 // TODO: Add menu that lets you set up a timer and saves it to a file
 // class AddTimerMenu extends Component {}
