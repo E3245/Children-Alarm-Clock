35cc9c295cc62dc17a46ea80e82c6fdadaff0e63
jl0844 <5272660+TheJohnLong@users.noreply.github.com>
2020-11-19 13:36:07 -0600

    remove unused imports

--- components/Timer/EditTimerModal.tsx
+++ components/Timer/EditTimerModal.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Button, ScrollView, StyleSheet} from 'react-native';
-import {Text, View, styles} from '../stylesheet';
+import {View} from '../stylesheet';
 import Modal from 'react-native-modal';
 import EditTimer from './EditTimer';
 import {TimerProps} from '../Timer/Timer';
