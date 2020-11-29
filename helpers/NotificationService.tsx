ca1ff6cce9cac66ee3c1f945fc35a5ab24de40ec
E3245 <gibbly@zoho.com>
2020-11-29 12:41:04 -0600

    Changed several methods of the notification service, and added hardcoded channel-ids

--- helpers/NotificationService.tsx
+++ helpers/NotificationService.tsx
@@ -4,7 +4,14 @@
 import PushNotification from 'react-native-push-notification';
 import NotificationHandler from './NotificationHandler';
 
+export const NOTIFICATION_CHANNEL_TIMER = 'child-friendly-alarm-clock-timer-id';
+export const NOTIFICATION_CHANNEL_ALARM = 'child-friendly-alarm-clock-alarm-id';
+
 export default class NotifService {
+  static checkPermission: any;
+  lastId: number;
+  lastChannelCounter: number;
+
   constructor(onRegister, onNotification) {
     this.lastId = 0;
     this.lastChannelCounter = 0;
@@ -29,28 +36,33 @@ export default class NotifService {
   createDefaultChannels() {
     PushNotification.createChannel(
       {
-        channelId: "default-channel-id", // (required)
-        channelName: `Default channel`, // (required)
-        channelDescription: "A default channel", // (optional) default: undefined.
+        channelId: NOTIFICATION_CHANNEL_TIMER, // (required)
+        channelName: `Timer channel`, // (required)
+        channelDescription: "Channel explicitily used for timers", // (optional) default: undefined.
         soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
         importance: 4, // (optional) default: 4. Int value of the Android notification importance
         vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
       },
-      (created) => console.log(`createChannel 'default-channel-id' returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+      (created) => {
+          console.log(`'child-friendly-alarm-clock-timer-id' channel created`) 
+      } // (optional) callback returns whether the channel was created, false means it already existed.
     );
     PushNotification.createChannel(
       {
-        channelId: "sound-channel-id", // (required)
-        channelName: `Sound channel`, // (required)
-        channelDescription: "A sound channel", // (optional) default: undefined.
-        soundName: "sample.mp3", // (optional) See `soundName` parameter of `localNotification` function
+        channelId: NOTIFICATION_CHANNEL_ALARM, // (required)
+        channelName: `Alarm channel`, // (required)
+        channelDescription: "Channel explicitily used for alarms", // (optional) default: undefined.
+        soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
         importance: 4, // (optional) default: 4. Int value of the Android notification importance
         vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
       },
-      (created) => console.log(`createChannel 'sound-channel-id' returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+      (created) => {
+          console.log(`'child-friendly-alarm-clock-alarm-id' channel created`) 
+      } // (optional) callback returns whether the channel was created, false means it already existed.
     );
   }
 
+  /*
   createOrUpdateChannel() {
     this.lastChannelCounter++;
     PushNotification.createChannel(
@@ -65,6 +77,7 @@ export default class NotifService {
       (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
     );
   }
+  */
 
   popInitialNotification() {
     PushNotification.popInitialNotification((notification) => console.log('InitialNotication:', notification));
@@ -110,19 +123,20 @@ export default class NotifService {
     });
   }
 
-  scheduleNotif(soundName) {
+  scheduleNotificationTimer(A_channelID: string, U_title: string, U_msg: string, A_bigText: string, A_smallTxt: string, trigger_date: Date, soundName: string) {
     this.lastId++;
     PushNotification.localNotificationSchedule({
-      date: new Date(Date.now() + 30 * 1000), // in 30 secs
+      date: trigger_date,
 
       /* Android Only Properties */
-      channelId: soundName ? 'sound-channel-id' : 'default-channel-id',
+      channelId: A_channelID,
       ticker: 'My Notification Ticker', // (optional)
-      autoCancel: true, // (optional) default: true
+      autoCancel: false, // (optional) default: true
       largeIcon: 'ic_launcher', // (optional) default: "ic_launcher"
       smallIcon: 'ic_notification', // (optional) default: "ic_notification" with fallback for "ic_launcher"
-      bigText: 'My big text that will be shown when notification is expanded', // (optional) default: "message" prop
-      subText: 'This is a subText', // (optional) default: none
+
+      bigText: A_bigText, // (optional) default: "message" prop
+      subText: A_smallTxt, // (optional) default: none
       color: 'blue', // (optional) default: system default
       vibrate: true, // (optional) default: true
       vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
@@ -130,11 +144,14 @@ export default class NotifService {
       group: 'group', // (optional) add group to message
       groupSummary: false, // (optional) set this notification to be the group summary for a group of notifications, default: false
       ongoing: false, // (optional) set whether this is an "ongoing" notification
-      actions: ['Yes', 'No'], // (Android only) See the doc for notification actions to know more
-      invokeApp: false, // (optional) This enable click on actions to bring back the application to foreground or stay in background, default: true
+      //actions: ['Dismiss'], // (Android only) See the doc for notification actions to know more
 
-      when: null, // (optionnal) Add a timestamp pertaining to the notification (usually the time the event occurred). For apps targeting Build.VERSION_CODES.N and above, this time is not shown anymore by default and must be opted into by using `showWhen`, default: null.
-      usesChronometer: false, // (optional) Show the `when` field as a stopwatch. Instead of presenting `when` as a timestamp, the notification will show an automatically updating display of the minutes and seconds since when. Useful when showing an elapsed time (like an ongoing phone call), default: false.
+      invokeApp: false, // (optional) This enable click on actions to bring back the application to foreground or stay in background, default: true
+      ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear). should be used in combine with `com.dieam.reactnativepushnotification.notification_foreground` setting
+
+      showWhen: true,
+      when: new Date(Date.now() + 3 * 1000), // (optionnal) Add a timestamp pertaining to the notification (usually the time the event occurred). For apps targeting Build.VERSION_CODES.N and above, this time is not shown anymore by default and must be opted into by using `showWhen`, default: null.
+      usesChronometer: true, // (optional) Show the `when` field as a stopwatch. Instead of presenting `when` as a timestamp, the notification will show an automatically updating display of the minutes and seconds since when. Useful when showing an elapsed time (like an ongoing phone call), default: false.
       timeoutAfter: null, // (optional) Specifies a duration in milliseconds after which this notification should be canceled, if it is not already canceled, default: null
     
       /* iOS only properties */
@@ -143,13 +160,18 @@ export default class NotifService {
       
       /* iOS and Android properties */
       id: this.lastId, // (optional) Valid unique 32 bit integer specified as string. default: Autogenerated Unique ID
-      title: 'Scheduled Notification', // (optional)
-      message: 'My Notification Message', // (required)
+      title: U_title, // (optional)
+      message: U_msg, // (required)
       userInfo: { sceen: "home" }, // (optional) default: {} (using null throws a JSON value '<null>' error)
       playSound: !!soundName, // (optional) default: true
       soundName: soundName ? soundName : 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
       number: 10, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
     });
+
+    console.log(`Created Notification with ID '${this.lastId}'`)
+
+    // Return the ID that was created for this notification
+    return this.lastId;
   }
 
   checkPermission(cbk) {
@@ -162,6 +184,11 @@ export default class NotifService {
 
   cancelNotif() {
     PushNotification.cancelLocalNotifications({id: '' + this.lastId});
+  }
+
+  cancelSpecificNotif(id: number)
+  {
+    PushNotification.cancelLocalNotifications({id: '' + id});   
   }
 
   cancelAll() {
